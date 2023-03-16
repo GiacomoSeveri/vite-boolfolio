3e53ecfd6b2cc0f0cae8b633926a5b2954af7b14
GicomoSeveri <giacomo.severi.sg@gmail.com>
2023-03-16 19:02:19 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script>
+import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
+import ProjectList from './components/ProjectList.vue';
+const apiBaseUrl = 'http://localhost:8000/api/';
+export default {
+  name: 'App',
+  components: { AppHeader, ProjectList },
+  data: () => ({
+    projects: []
+  }),
+  methods: {
+    callProjects() {
+      axios.get(apiBaseUrl + 'project').then(res => {
+        this.projects = res.data;
+      })
+    }
+  },
+  created() {
+    this.callProjects();
+  }
+};
+</script>
+
+<template>
+  <AppHeader />
+  <main class="container">
+    <ProjectList :projects="projects" />
+  </main>
+</template>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,19 @@
+<script>
+
+export default {
+    name: 'ProjectList',
+    props: { projects: Array }
+};
+</script>
+
+<template>
+    <main class="container">
+        <div class="my-5">
+            <ul>
+                <li v-for="project in projects" :kay="project.id">
+                    {{ project.title }}
+                </li>
+            </ul>
+        </div>
+    </main>
+</template>
